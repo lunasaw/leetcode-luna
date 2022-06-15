6c1a3b2fc82d108d7b01732ca042d1b05fe761f3
luna <iszychen@gmail.com>
2022-06-15 20:48:16 +0800

    303. 区域和检索 - 数组不可变

--- src/main/java/linearlist/theprefixand/prefixandbase/RangeSumQueryImmutable303.java
+++ src/main/java/linearlist/theprefixand/prefixandbase/RangeSumQueryImmutable303.java
@@ -45,4 +45,21 @@ public class RangeSumQueryImmutable303 {
     public static void main(String[] args) {
 
     }
+
+    class NumArray {
+
+        int[] sums;
+
+        public NumArray(int[] nums) {
+            int n = nums.length;
+            sums = new int[n + 1];
+            for (int i = 0; i < nums.length; i++) {
+                sums[i + 1] = sums[i] + nums[i];
+            }
+        }
+
+        public int sumRange(int left, int right) {
+            return sums[right + 1] - sums[left];
+        }
+    }
 }
